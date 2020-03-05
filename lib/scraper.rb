5c1d0334e497bcd7c23106a18d75f497368ec59b
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-05 05:33:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,7 +2,21 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-  
+  def self.scrape_index_page(index_url)
+    page = Nokogiri::HTML(open(index_url))
+    students = []
+
+    page.css("div.student-card").each do |student|
+      name = student.css(".student-name").text
+      location = student.css(".student-location").text
+      profile_url = student.css("a").attribute("href").value
+      student_info = {:name => name,
+                :location => location,
+                :profile_url => profile_url}
+      students << student_info
+      end
+    students
+   end
 
  def self.scrape_profile_page(profile_url)
     student_profile = {}
