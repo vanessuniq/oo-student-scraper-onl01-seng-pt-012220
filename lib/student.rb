2a14977ad00dedbaa86dde9f48ba06a9e9a3e316
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-05 05:39:02 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,26 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each {|student_hash| Student.new(student_hash)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    self.send("twitter=", attributes_hash[:twitter])
+    self.send("linkedin=", attributes_hash[:linkedin])
+    self.send("github=", attributes_hash[:github])
+    self.send("blog=", attributes_hash[:blog])
+    self.send("profile_quote=", attributes_hash[:profile_quote])
+    self.send("bio=", attributes_hash[:bio])
   end
 
   def self.all
-    
+    @@all
   end
+  
 end
 
